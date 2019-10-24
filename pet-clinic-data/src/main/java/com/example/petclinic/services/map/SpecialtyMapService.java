f7850a7a2b96fe09ca2f4a0ef8d826b40260ab4b
blaise <ffringel9@gmail.com>
2019-10-23 22:44:27 -0500

    closes #49

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/SpecialtyMapService.java
@@ -2,11 +2,13 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Specialty;
 import com.example.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtyService {
     @Override
     public Set<Specialty> findAll() {
--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VetMapService.java
@@ -2,11 +2,13 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
